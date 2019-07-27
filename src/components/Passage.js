2b32850ce982d182e31c46280bd81d68df4d9101
Penny Shen <penny@medium.com>
2019-07-27 14:34:01 -0700

    add profile into passage

--- src/components/Passage.js
+++ src/components/Passage.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import Conversation from './Conversation';
+import './components.css';
+import Profile from './Profile';
 
 class Passage extends React.Component {
   render() {
     const script = require('../scripts/script1')
     const startingId = 1
+    const profiles = require('../scripts/profiles')
 
     return (
-      <Conversation
-        script={script}
-        startingId={startingId}
-        myName="Grace"
-      />
+      <div className="conversationContainer">
+        <Profile
+          profile={profiles[2]}
+        />
+        <Conversation
+          script={script}
+          startingId={startingId}
+          myName="Grace"
+        />
+      </div>
     )
   }
 }
