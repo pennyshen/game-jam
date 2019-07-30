eb96fbdc57616abe65b1ed71561e1089974004e8
Penny Shen <penny@medium.com>
2019-07-29 22:34:15 -0700

    Updates

---
+++ precache-manifest.d5fcce070367a5694878d98b22e1180b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8171c069e84a3194455ef21c829616a",
+    "url": "/game-jam/index.html"
+  },
+  {
+    "revision": "358ca98db339c1c832a3",
+    "url": "/game-jam/static/css/main.4a90405d.chunk.css"
+  },
+  {
+    "revision": "8b40bc9af786e24202c0",
+    "url": "/game-jam/static/js/2.ad19c478.chunk.js"
+  },
+  {
+    "revision": "358ca98db339c1c832a3",
+    "url": "/game-jam/static/js/main.1281ce33.chunk.js"
+  },
+  {
+    "revision": "7437032db67c6f911e12",
+    "url": "/game-jam/static/js/runtime~main.b23950a4.js"
+  }
+]);
